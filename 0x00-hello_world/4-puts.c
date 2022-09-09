b74d2c4496268ce33159ce98323bae34719f2e4f
Seriki Olawale <olawale_seriki@yahoo.com>
2022-09-08 17:39:06 -0700

    execute

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-|^
  * 
  * main - entry point
  *
  * Return: 0 after printing the function
  * /
  int main(void)
- {puts("\"programming is like building a multilingual puzzle");
- 	Return (0)
+ {
+ 	puts("\"programming is like building a multilingual puzzle");
+ 	Return (0);
 }
