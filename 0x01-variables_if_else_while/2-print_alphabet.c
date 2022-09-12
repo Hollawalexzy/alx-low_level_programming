7d16903d72b00798074e4dad5b9f5856187de694
Seriki Olawale <olawale_seriki@yahoo.com>
2022-09-12 11:48:54 -0700

    vommti

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main - print if the number is positive, zero, or negative
  *
- * Description: using the main function 
+ * Description: using the main function
  * this program prints "programming is positive, zero, or negative"
  * Return: 0
  */
