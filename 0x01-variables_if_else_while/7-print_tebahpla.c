c1d90060d6dfe3f903e7622ede251392ef28075d
Seriki Olawale <olawale_seriki@yahoo.com>
2022-09-12 11:13:57 -0700

    numbers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print the lowercase alphabet in reverse
+ *
+ * Description: using the main function
+ * this program prints the lowercase alphabet in reverse
+ * using only the putchar function
+ * return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c>= 'a' c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all the number of base 16 in lower case
+ *
+ * Description: using the main function
+ * this program prints all the number of base 16 in lower case
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c<= '9'; c++)
+{
+	putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
