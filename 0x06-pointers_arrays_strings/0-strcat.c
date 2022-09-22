ba9893b3d53f97a22758e0631148615640049b99
Seriki Olawale <olawale_seriki@yahoo.com>
2022-09-22 13:37:49 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - A function that concatenates two strings
+ * @dest: string to be appended to
+ * @src: string to add
+ * Return: Dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+		int i, j;
+
+		i = 0;
+		j = i0;
+
+		while (dest[i] != '\0')
+			i++;
+
+		while (src[j] != '\0')
+		{
+			dest[i] = src[j];
+			j++;
+		}
+		dest[i] = '\0'
+
+		return (dest);
+}
