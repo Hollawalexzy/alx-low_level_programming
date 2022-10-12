04e9605137457f446932b4451d18e389b1ded318
Seriki Olawale <olawale_seriki@yahoo.com>
2022-10-12 13:24:39 -0700

    commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name of the person
+ * @f: function to print with
  *
  * Return: Void.
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-		f(name)
+		f(name);
 }
